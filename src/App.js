7604874bb521189a37f816ac9a6b7780e5547400
twismdx <twism@live.com>
2022-09-01 14:47:51 +0930

    master

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import { useState, useEffect } from 'react';
-import './App.css';
-import Leaders from './Leaders';
-
-const API_LEADER = './leader.json'
+import '../App.css';
+import Leaders from '../JSX Components/Leaders';
+import title from './Scoreboard';
 
 function App() {
 const [ rankings, setRankings ] = useState([]);
 
+var myHeaders = new Headers();
+myHeaders.append("Ocp-Apim-Subscription-Key", "a5a933d50f7b40928d1e0c0612903033");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
 const getLeaderboard = async () => {
-    const response = await fetch (`${API_LEADER}`);
-    const data = await response.json();
+const response = await fetch("https://t140apim.azure-api.net/demoT140LivestreamApi/GetLeaderboard?T140EventId=499cd369-8583-4fad-92dc-14b5d48ab445", requestOptions)
+const data = await response.json();
 
     setRankings(data);
   }
@@ -20,7 +28,7 @@ const getLeaderboard = async () => {
 
   return ( 
     <div className="leaderboard">
-      <Leaders rankings={rankings} />
+      <Leaders rankings={rankings} title={title} />
     </div>
   )
 }
