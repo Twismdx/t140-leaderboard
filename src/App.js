a6f0371eec587f2526a8826e1c9f303aa726c47c
twismdx <twism@live.com>
2022-09-08 18:55:51 +0930

    newmaster

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
-import Leaderboard from './Components/Leaderboard';
 import $ from 'jquery';
+import Leaders from './JSXComponents/Leaders';
+import title from './Components/Scoreboard';
 
 function App() {
         
@@ -33,5 +34,47 @@ function App() {
         </div>
     )
 }
+
+function LeaderboardChild({ eventId }) {
+
+    return (
+      <>
+        {eventId.map(eventId => (
+          <li key={eventId.id}>{eventId.name}</li>
+        ))}
+      </>  
+    );
+  }
+  
+  function LeaderBoard(eventId) {
+  
+    const [ rankings, setRankings ] = useState([]);
+  
+    useEffect(() => { 
+  
+      var params = eventId;
+      var urlPrefix = "https://t140apim.azure-api.net/demoT140LivestreamApi/GetScores?T140EventId=";
+      var url = urlPrefix + encodeURIComponent(params)
+    
+          $.ajax({
+          url: url,
+          headers: {
+            "Ocp-Apim-Subscription-Key": "a5a933d50f7b40928d1e0c0612903033"
+          },
+          type: "GET",
+          dataType: "json",
+        })
+        .then(function (data) {
+            setRankings(data)
+        })
+        }, [eventId])
+    
+    return ( 
+  
+      <div className="leaderboard">
+        <Leaders rankings={rankings} title={title} />
+      </div>
+    )
+  }
   
   export default App;
